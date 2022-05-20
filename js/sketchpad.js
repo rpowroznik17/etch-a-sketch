4c1744aea132bedca744828f9ebc230f1f16e067
Rafal <rafalp443@gmail.com>
2022-05-20 15:56:00 +0200

    Add basic functionalities of drawing editors
    Added functionality to pick a desired color, erase, clear the
    whole canvas and change it's size.

--- js/sketchpad.js
+++ js/sketchpad.js
@@ -1,16 +1,29 @@
 let isMouseDown = false;
+let colorPicker;
+let defaultColor = '#4287f5';
+let currentColor = defaultColor;
+
+window.addEventListener('load', createBoard(16));
+window.addEventListener('load', setupColorPicker);
 
 const creationBtn = document.querySelector('#creation-btn');
-creationBtn.addEventListener('click', createNewBoard);
+creationBtn.addEventListener('click', changeSizeOfTheBoard);
 
-function createNewBoard() {
-    if (checkIfBoardExists()) {
-        const board = document.querySelector('.board');
-        board.remove();
-    }
+const clearBtn = document.querySelector('#clear-btn');
+clearBtn.addEventListener('click', clearTheBoard);
 
-    const sideLength = prompt('Enter the length of side of the sketchpad: ');
+const rubberBtn = document.querySelector('#rubber-btn');
+rubberBtn.addEventListener('click', () => {
+    currentColor = '#fff';
+});
 
+// const drawBtn = document.querySelector('#draw-btn');
+// drawBtn.addEventListener('click', () => {
+//     drawingColor = currentColor;
+// });
+
+
+function createBoard(sideLength) {
     const board = document.createElement('div');
     board.classList.add('board');
     board.style['grid-template-columns'] = `repeat(${sideLength}, 1fr)`;
@@ -19,16 +32,57 @@ function createNewBoard() {
     board.addEventListener('mouseup', stopDrawing);
     
     fillBoardWithEmptyFields(sideLength, board);
-    document.body.appendChild(board); 
+    document.body.appendChild(board);
 }
 
-function checkIfBoardExists() {
+function changeSizeOfTheBoard() {
     const board = document.querySelector('.board');
-    if (board === null) {
-        return false;
-    }
-    return true;
+    board.remove()
+    const sideLength = prompt('Enter the length of side of the sketchpad: ');
+    createBoard(sideLength);
 }
+
+function clearTheBoard() {
+    const fields = document.querySelectorAll('.board-field');
+    fields.forEach(field => field.style['background-color'] = '#fff');
+}
+
+function setupColorPicker() {
+    colorPicker = document.querySelector('#colorPicker');
+    colorPicker.value = defaultColor;
+    colorPicker.addEventListener('input', changeColor);
+}
+
+function changeColor(e) {
+    currentColor = e.target.value;
+}
+
+// function createNewBoard() {
+//     if (checkIfBoardExists()) {
+//         const board = document.querySelector('.board');
+//         board.remove();
+//     }
+
+//     const sideLength = prompt('Enter the length of side of the sketchpad: ');
+
+//     const board = document.createElement('div');
+//     board.classList.add('board');
+//     board.style['grid-template-columns'] = `repeat(${sideLength}, 1fr)`;
+//     board.style['grid-template-rows'] = `repeat(${sideLength}, 1fr)`;
+//     board.addEventListener('mousedown', startDrawing);
+//     board.addEventListener('mouseup', stopDrawing);
+    
+//     fillBoardWithEmptyFields(sideLength, board);
+//     document.body.appendChild(board); 
+// }
+
+// function checkIfBoardExists() {
+//     const board = document.querySelector('.board');
+//     if (board === null) {
+//         return false;
+//     }
+//     return true;
+// }
 
 function fillBoardWithEmptyFields(sideLength, board) {
     const size = sideLength ** 2;
@@ -41,9 +95,9 @@ function fillBoardWithEmptyFields(sideLength, board) {
     }
 }
 
-function draw(e){
+function draw(e) {
     if (isMouseDown) {
-        e.target.style['background-color'] = '#000';
+        e.target.style['background-color'] = currentColor;
     }
 }
 
